08ec374873e8fb850d4de6219a11ff0d1238379b
niedongye <niedongye@efeiyi.com>
2018-06-27 18:03:31 +0800

    update

---
+++ my-app/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <app-index/>
+
+
+
+
+    <footer>
+      <div class="content">
+        <a @click="redirect(item.link)" class="link" :class="{active:index==selected}" v-for="item,index in footData">
+          <i :class="item.icon"></i>
+          <i v-if="index==2" class="icon-midbg"></i>
+          <div class="txt">{{item.name}}</div>
+        </a>
+      </div>
+    </footer>
+
+
+  </div>
+</template>
+
+<script>
+import appIndex from './components/activate/index'
+
+export default {
+  name: 'App',
+  components: {
+    appIndex
+  },
+  props: {
+    selected: 0
+  },
+  data: function () {
+    return {
+      drawerData: [
+        {name: "消息中心", link: "/pages/my/details.html"},
+        {name: "意见反馈", link: "/pages/my/details.html"},
+        {name: "设置", link: "/pages/my/setting.html"},
+      ],
+      footData: [
+        {name: "首页", icon: "icon-home", link: "/pages/index/index.html"},
+        {name: "资讯", icon: "icon-news", link: "/pages/news/index.html?channelId=0"},
+        {name: "百科", icon: "icon-cyclopedia", link: "/pages/ency/index.html"},
+        {name: "活动", icon: "icon-activity", link: "/pages/activity/index.html?channelId=1"},
+        {name: "活化馆", icon: "icon-activate", link: "/pages/activate/index.html"}
+      ]
+    }
+  },
+  methods: {
+    redirect(href){
+      sessionStorage.removeItem('indexData')
+      sessionStorage.removeItem('newsData')
+      sessionStorage.removeItem('activityData')
+      window.location.href = href
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ my-app/src/components/activate/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    内容
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'appIndex',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
